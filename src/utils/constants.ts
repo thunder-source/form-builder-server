b6bab219ecbfda3470e0a1b6a403c1491bb7cf46
thunder-source <majhipraditya@gmail.com>
2025-06-11 10:58:07 +0530

    Refactor constants for consistency and add new allowed origins

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,20 +1,22 @@
-import type { CookieOptions } from 'express';
+import type { CookieOptions } from "express";
 
 export const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:4173',
-  'http://127.0.0.1:5173',
-  'https://forms-api.pradityamanjhi.site',
-  'https://forms.pradityamanjhi.site',
-  'https://form-builder-client-rosy.vercel.app',
+  "http://localhost:5173",
+  "http://localhost:4173",
+  "http://127.0.0.1:5173",
+  "https://forms-api.pradityamanjhi.site",
+  "https://forms-api.pradityamanjhi.in",
+  "https://forms.pradityamanjhi.site",
+  "https://forms.pradityamanjhi.in",
+  "https://form-builder-client-rosy.vercel.app",
 ];
 
-export const accessTokenExpiresIn = '1h';
-export const refreshTokenExpiresIn = '7d';
+export const accessTokenExpiresIn = "1h";
+export const refreshTokenExpiresIn = "7d";
 
 export const cookieOptions: CookieOptions = {
   httpOnly: true,
-  sameSite: 'none',
+  sameSite: "none",
   secure: true,
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
